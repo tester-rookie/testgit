15eab96928653ee69f1bdc80ca2d648aa31e0b2e
tester-rookie <1761720237@163.com>
2022-10-18 17:42:57 +0800

    增加base64加密算法

--- study/Study_delivery_system/Utils/handle_data.py
+++ study/Study_delivery_system/Utils/handle_data.py
@@ -3,6 +3,7 @@
 # @file    : handle_data.py
 
 import hashlib
+import base64
 
 def get_md5(values:str):
     """
@@ -13,4 +14,20 @@ def get_md5(values:str):
     md5=hashlib.md5()
     md5.update(values.encode('utf-8'))
     return md5.hexdigest()
+
+def get_base64(values:str):
+    '''
+
+    :param values:
+    :return:
+    '''
+    # a = input("base64加密：")  # input()函数接收任意任性输入，将所有输入默认为字符串处理，并返回字符串类型。
+    encode = base64.b64encode(values.encode('utf-8'))  # 因为3.x中字符都为unicode编码，而b64encode函数的参数为byte类型，所以必须先转码
+    print(("**********encryption complete**********\n:"), str(encode, 'utf-8'))  # 将byte转换回去就好了，如果不转的化，输出结果会被字符串b包围
+    return str(encode,'utf-8')
+
+
+if __name__ == '__main__':
+    ret=get_base64('billapp')
+    print(ret)
 
